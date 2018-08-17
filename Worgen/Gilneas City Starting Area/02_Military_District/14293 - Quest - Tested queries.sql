dfdc2aa9dfe8da5bfeee0ad95a031e3be7624ea9
wormogo <34318992+wormogo@users.noreply.github.com>
2018-08-17 19:12:34 +0200

    Krennan_Aranas_on_tree_SAI_added
    Co-Authored-By: monady <monady@users.noreply.github.com>

--- Worgen/Gilneas City Starting Area/02_Military_District/14293 - Quest - Tested queries.sql
+++ Worgen/Gilneas City Starting Area/02_Military_District/14293 - Quest - Tested queries.sql
@@ -18,5 +18,13 @@ DELETE FROM `smart_scripts` WHERE `entryorguid`= 35550 AND `source_type`=0;
 UPDATE creature_template SET AIName="SmartAI" WHERE entry= 35550 LIMIT 1;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
 (35550,0,0,0,1,0,100,0,30000,30000,0,0,1,0,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"King Genn Greymane - Out Of Combat - Say Text"),
-(35550,0,1,0,19,0,100,0,14293,0,0,0,75,49414,0,0,0,0,0,7,0,0,0,0.0,0.0,0.0,0.0,"King Genn Greymane - Accepted Quest - Add Aura 49416"),
+(35550,0,1,0,19,0,100,0,14293,0,0,0,75,49414,0,0,0,0,0,7,0,0,0,0.0,0.0,0.0,0.0,"King Genn Greymane - Accepted Quest - Add Aura 49414"),
 (35550,0,2,1,61,0,100,0,0,0,0,0,11,68232,0,0,0,0,0,7,0,0,0,0.0,0.0,0.0,0.0,"King Genn Greymane - Link - Cast 68232");
+
+-- Krennan Aranas on tree 35753
+UPDATE `creature_template` SET `AIName`='SmartAI', `ScriptName`='', `unit_flags`=33280, `flags_extra`=2 WHERE `entry` IN (35753);
+DELETE FROM `smart_scripts` WHERE `entryorguid` IN (35753);
+INSERT INTO `smart_scripts` VALUES
+(35753, 0, 0, 0, 1, 0, 100, 0, 0, 0, 60000, 60000, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Krennan Aranas - Out Of Combat - Say Text'),
+(35753, 0, 1, 0, 8, 0, 100, 1, 68219, 0, 0, 0, 0, 41, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Krennan Aranas - On Spellhit - Forced Despawn');
+
