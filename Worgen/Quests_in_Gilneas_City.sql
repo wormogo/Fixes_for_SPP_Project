87f02f655fa6ffb5d79a0510d42ba73c662fa620
wormogo <34318992+wormogo@users.noreply.github.com>
2018-08-17 11:56:56 +0200

    Quests_in_Gilneas_City
    Co-Authored-By: Monady <monady@users.noreply.github.com>

---
+++ Worgen/Quests_in_Gilneas_City.sql
@@ -0,0 +1,33 @@
+-- Worgen 
+
+-- Gilneas City 
+
+-- Merchant Square
+
+-- Lockdown! 
+-- Something's Amiss 
+-- All Hell Breaks Loose
+-- Evacuate The Merchant Square
+-- Salvage The Supplies
+-- Royal Orders
+
+-- Military District
+
+-- Someone's Looking For You
+-- Safety In Numbers
+-- Old Divisions
+-- The Prison Rooftop
+-- By The Skin Of His Teeth
+-- While You're At It
+-- Brothers In Arms
+-- The Rebel Lord's Arsenal
+-- From The Shadows
+-- Message To Greymane
+-- Save Krennan Aranas
+-- Time To Regroup 
+
+-- Cathedral Quarter
+
+-- By Blood And Ash
+-- Never Surrender, Sometimes Retreat
+-- Last Stand
