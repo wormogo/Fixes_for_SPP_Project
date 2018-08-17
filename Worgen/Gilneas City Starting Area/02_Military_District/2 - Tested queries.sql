be46e758bb57d7e24277b60192ff5a2fd0d5c595
wormogo <34318992+wormogo@users.noreply.github.com>
2018-08-17 16:49:39 +0200

    Add_Tested_Queries
    Co-Authored-By: monady <monady@users.noreply.github.com>

---
+++ Worgen/Gilneas City Starting Area/02_Military_District/2 - Tested queries.sql
@@ -0,0 +1,7 @@
+Tested:
+
+Save Krennan Aranas:
+
+INSERT INTO `vehicle_template_accessory` (`entry`, `accessory_entry`, `seat_id`, `minion`, `description`, `summontype`, `summontimer`) VALUES
+(35905, 35907, 1, 0, 35905 - 35907, 0, 0); -- 35905 - 35907
+
