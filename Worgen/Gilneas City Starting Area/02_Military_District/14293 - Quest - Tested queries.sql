7b168477601c11d1cb488f57ed9fc07838afd2d7
wormogo <34318992+wormogo@users.noreply.github.com>
2018-08-17 17:12:55 +0200

    Creature_template_35905_Tested
    Co-Authored-By: monady <monady@users.noreply.github.com>

--- Worgen/Gilneas City Starting Area/02_Military_District/14293 - Quest - Tested queries.sql
+++ Worgen/Gilneas City Starting Area/02_Military_District/14293 - Quest - Tested queries.sql
@@ -5,4 +5,4 @@ Save Krennan Aranas:
 INSERT INTO `vehicle_template_accessory` (`entry`, `accessory_entry`, `seat_id`, `minion`, `description`, `summontype`, `summontimer`) VALUES
 (35905, 35907, 1, 0, 35905 - 35907, 0, 0); -- 35905 - 35907
 
-
+UPDATE `creature_template` SET `AIName`='SmartAI', `ScriptName`='', `spell1`=68219, `unit_flags`=10 WHERE `entry` IN (35905);
