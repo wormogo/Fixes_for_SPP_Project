f547c4fd329595ce6023122622250066c2b11e06
Monady <vladroman@ymail.com>
2018-08-23 13:28:58 +0300

    Update 2018_08_22_00_fix_-_35369_josiah_avery_corect_emote.sql

--- EasternKingdoms/Gilneas/Gilneas City Starting Area/02_Military_District/Military_District_Fixes/2018_08_22_00_fix_-_35369_josiah_avery_corect_emote.sql
+++ EasternKingdoms/Gilneas/Gilneas City Starting Area/02_Military_District/Military_District_Fixes/2018_08_22_00_fix_-_35369_josiah_avery_corect_emote.sql
@@ -1,3 +1,4 @@
 -- fix - corect emotes 35369 josiah avery
-UPDATE creature_addon SET emote=375 WHERE guid=20556362;
-UPDATE creature_addon SET emote=431 WHERE guid=20556546;
+DELETE FROM `creature_addon` WHERE `guid`=20556546;
+INSERT INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `aiAnimKit`, `movementAnimKit`, `meleeAnimKit`, `auras`) 
+VALUES (20556546, 0, 0, 65536, 1, 431, 0, 0, 0, 'null');
