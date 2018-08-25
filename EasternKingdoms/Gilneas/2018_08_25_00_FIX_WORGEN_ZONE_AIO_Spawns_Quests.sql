a2c17156900858bfa696af99441509b1f53a2ce1
wormogo <34318992+wormogo@users.noreply.github.com>
2018-08-25 18:54:22 +0200

    Phase Fix Duskhaven and Creature Model Fix
    Co-Authored-By: monady <monady@users.noreply.github.com>

--- EasternKingdoms/Gilneas/2018_08_25_00_FIX_WORGEN_ZONE_AIO_Spawns_Quests.sql
+++ EasternKingdoms/Gilneas/2018_08_25_00_FIX_WORGEN_ZONE_AIO_Spawns_Quests.sql
@@ -8589,3 +8589,14 @@ INSERT INTO `npc_spellclick_spells` (`npc_entry`, `spell_id`, `cast_flags`, `use
 (38615, 72472, 0, 0),
 (43747, 81877, 0, 0),
 (53522, 99339, 0, 0);
+
+-- Phase Fixes
+
+-- fix - correct phase shifts duskhaven
+DELETE FROM `spell_area` WHERE `spell`=68481 AND `area`=4786 AND `quest_start`=14375 AND `aura_spell`=0 AND `teamId`=-1 AND `racemask`=0 AND `gender`=2;
+INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_end`, `aura_spell`, `teamId`, `racemask`, `gender`, `flags`, `quest_start_status`, `quest_end_status`) 
+VALUES (68481, 4786, 14375, 14321, 0, -1, 0, 2, 3, 64, 64);
+
+DELETE FROM `spell_area` WHERE `spell`=68482 AND `area`=4786 AND `quest_start`=14321 AND `aura_spell`=0 AND `teamId`=-1 AND `racemask`=0 AND `gender`=2;
+INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_end`, `aura_spell`, `teamId`, `racemask`, `gender`, `flags`, `quest_start_status`, `quest_end_status`) 
+VALUES (68482, 4786, 14321, 0, 0, -1, 0, 2, 3, 64, 0);
