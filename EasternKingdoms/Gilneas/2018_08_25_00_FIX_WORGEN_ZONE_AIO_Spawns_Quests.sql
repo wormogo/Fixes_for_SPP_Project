29e09b3bcf94b05447050599cb04969c66ea122f
wormogo <34318992+wormogo@users.noreply.github.com>
2018-08-25 18:40:55 +0200

    Delete_from_added_to_query
    Arrrrrggghhhhh

--- EasternKingdoms/Gilneas/2018_08_25_00_FIX_WORGEN_ZONE_AIO_Spawns_Quests.sql
+++ EasternKingdoms/Gilneas/2018_08_25_00_FIX_WORGEN_ZONE_AIO_Spawns_Quests.sql
@@ -8523,13 +8523,15 @@ INSERT INTO `quest_visual_effect` (`ID`, `Index`, `VisualEffect`, `VerifiedBuild
 (265467, 0, 1976, 25549),
 (268950, 0, 988, 25549);
 
--- VEHICLE TEMPLATE ACCESSORIES
+--- VEHICLE TEMPLATE ACCESSORIES
 
+DELETE FROM vehicle_template_accessory WHERE npc_entry IN (36283, 35905, 35231);
 INSERT INTO `vehicle_template_accessory` (`entry`, `accessory_entry`, `seat_id`, `minion`, `description`, `summontype`, `summontimer`) VALUES
 (36283, 36292, 2, 0, '36283 - 36292', 0, 0), -- 36283 - 36292
 (35905, 35907, 1, 0, '35905 - 35907', 3, 5000), -- 35905 - 35907
 (35231, 35230, 1, 0, '35231 - 35230', 3, 5000); -- 35231 - 35230
 
+DELETE FROM vehicle_template_accessory WHERE npc_entry IN (37939, 38363, 44928, 38755, 43336, 43337);
 INSERT INTO `vehicle_template_accessory` (`entry`, `accessory_entry`, `seat_id`, `minion`, `description`, `summontype`, `summontimer`) VALUES
 (37939, 37938, 0, 0, '37939 - 37938', 0, 0), -- 37939 - 37938
 (38363, 38366, 0, 0, '38363 - 38366', 0, 0), -- 38363 - 38366
@@ -8551,7 +8553,7 @@ INSERT INTO `vehicle_template_accessory` (`entry`, `accessory_entry`, `seat_id`,
 (44928, 37946, 5, 0, '44928 - 37946', 0, 0); -- 44928 - 37946
 
 -- SPELLCLICK SPELLS
-
+DELETE FROM npc_spellclick_spells WHERE npc_entry IN (35317, 44427,44429, 36283, 36287, 36288, 36289);
 INSERT INTO `npc_spellclick_spells` (`npc_entry`, `spell_id`, `cast_flags`, `user_type`) VALUES
 (35317, 43671, 0, 0),
 (44427, 56685, 0, 0),
@@ -8561,6 +8563,7 @@ INSERT INTO `npc_spellclick_spells` (`npc_entry`, `spell_id`, `cast_flags`, `use
 (36288, 68598, 0, 0),
 (36287, 68597, 0, 0);
 
+DELETE FROM npc_spellclick_spells WHERE npc_entry IN (36440, 36459, 36540, 44928, 38755, 37807, 38377, 38150, 38615, 43747, 53522);
 INSERT INTO `npc_spellclick_spells` (`npc_entry`, `spell_id`, `cast_flags`, `user_type`) VALUES
 (36440, 68735, 0, 0),
 (36459, 68743, 0, 0),
@@ -8568,6 +8571,11 @@ INSERT INTO `npc_spellclick_spells` (`npc_entry`, `spell_id`, `cast_flags`, `use
 (44928, 72767, 0, 0),
 (38755, 72767, 0, 0),
 (37807, 69434, 0, 0),
+(38377, 69434, 0, 0),
+(38150, 71238, 0, 0),
+(38615, 72472, 0, 0),
+(43747, 81877, 0, 0),
+(53522, 99339, 0, 0);
 (38377, 69434, 0, 0),
 (38150, 71238, 0, 0),
 (38615, 72472, 0, 0),
