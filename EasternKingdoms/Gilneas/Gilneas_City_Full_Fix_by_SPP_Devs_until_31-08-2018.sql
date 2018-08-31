d86ee3414529f29df5fd80f16ee2f68f5cf0f4df
wormogo <pehe.teszt@gmail.com>
2018-08-31 15:15:53 +0200

    Revert_AIO_and_delete_single_fix_which_added_to_AIO

--- EasternKingdoms/Gilneas/Gilneas_City_Full_Fix_by_SPP_Devs_until_31-08-2018.sql
+++ EasternKingdoms/Gilneas/Gilneas_City_Full_Fix_by_SPP_Devs_until_31-08-2018.sql
@@ -9043,13 +9043,5 @@ INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_end`, `aura_spe
 VALUES (59073, 4756, 14078, 14159, 0, -1, 0, 2, 3, 64, 64);
 
 
--- fix - corect models for captive worgen 36698 26797 36798 
-
-UPDATE creature_template SET modelid1=30294 AND modelid2=30293 AND modelid3=30295 AND modelid4=0 WHERE entry IN (36698); 
-UPDATE creature_template SET modelid1=30295 AND modelid2=30294 AND modelid3=30293 AND modelid4=0 WHERE entry IN (36698); 
-UPDATE creature_template SET modelid1=30293 AND modelid2=30295 AND modelid3=30294 AND modelid4=0 WHERE entry IN (36698);  
-UPDATE creature_template SET modelid1=30293 AND modelid2=30294 AND modelid3=30295 AND modelid4=0 WHERE entry IN (36797); 
-UPDATE creature_template SET modelid1=30295 AND modelid2=30294 AND modelid3=30293 AND modelid4=0 WHERE entry IN (36798); 
-
 -- End of various fixes
 
