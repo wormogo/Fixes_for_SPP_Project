e543b584d6edfaf05a937ddc31d7ca9dd7f606ce
wormogo <pehe.teszt@gmail.com>
2018-08-31 14:41:16 +0200

    Bridge_fix_gilneas_city_has_been_added_to_AIO
    Co-Authored-By: monady <monady@users.noreply.github.com>

--- EasternKingdoms/Gilneas/Gilneas_City_Full_Fix_by_SPP_Devs_until_31-08-2018.sql
+++ EasternKingdoms/Gilneas/Gilneas_City_Full_Fix_by_SPP_Devs_until_31-08-2018.sql
@@ -9036,5 +9036,12 @@ INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_end`, `aura_spe
 VALUES (49416, 4756, 14078, 14099, 0, -1, 0, 2, 3, 64, 10);
 
 
+-- fix - bridge phase corections between merchant square and military district - by Monad
+
+DELETE FROM `spell_area` WHERE `spell`=59073 AND `area`=4755 AND `quest_start`=14078 AND `aura_spell`=0 AND `teamId`=-1 AND `racemask`=0 AND `gender`=2;
+INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_end`, `aura_spell`, `teamId`, `racemask`, `gender`, `flags`, `quest_start_status`, `quest_end_status`) 
+VALUES (59073, 4756, 14078, 14159, 0, -1, 0, 2, 3, 64, 64);
+
+
 -- End of various fixes
 
