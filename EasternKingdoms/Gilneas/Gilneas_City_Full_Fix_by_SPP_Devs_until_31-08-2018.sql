c0ad5dfea976fe7cc048a507d2a71f5cb8e79c8c
Monady <vladroman@ymail.com>
2018-09-02 16:49:44 +0300

    phase fix
    corected phase shift outside duskhaven

--- EasternKingdoms/Gilneas/Gilneas_City_Full_Fix_by_SPP_Devs_until_31-08-2018.sql
+++ EasternKingdoms/Gilneas/Gilneas_City_Full_Fix_by_SPP_Devs_until_31-08-2018.sql
@@ -128,6 +128,7 @@ VALUES (68482, 4786, 14321, 0, 0, -1, 0, 2, 3, 64, 0);
 DELETE FROM `spell_area` WHERE `spell`=68482 AND `area`=4714 AND `quest_start`=14336 AND `aura_spell`=0 AND `teamId`=-1 AND `racemask`=0 AND `gender`=2;
 INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_end`, `aura_spell`, `teamId`, `racemask`, `gender`, `flags`, `quest_start_status`, `quest_end_status`) 
 VALUES (68482, 4714, 14321, 0, 0, -1, 0, 2, 3, 64, 0);
+-- end - corect phase shifts duskhaven
 
 
 -- Worgen vehicle template accesory and NPC Splellclick Spells fix by Wormogo
