c81f9f758a51729e1ea72fd2c6f5854233288885
Monady <vladroman@ymail.com>
2018-09-19 18:51:26 +0300

    some corrections for map changes gilneas duskhaven

--- 10-SQL/EasternKingdoms/Gilneas/DuskHaven/2018_09_19_00_fix - map changes in gilneas.sql
+++ 10-SQL/EasternKingdoms/Gilneas/DuskHaven/2018_09_19_00_fix - map changes in gilneas.sql
@@ -5,7 +5,7 @@ INSERT INTO terrain_swap_defaults (MapId, TerrainSwapMap, Comment) VALUES
 (654, 655, 'Gilneas duskhaven phase 1'),
 (654, 656, 'Gilneas duskhaven phase 2');
 
-DELETE FROM conditions WHERE SourceTypeOrReferenceId = 25 AND SourceEntry IN (655, 656);
+DELETE FROM conditions WHERE SourceTypeOrReferenceId = 25 AND SourceEntry IN (638, 655, 656);
 INSERT INTO conditions (SourceTypeOrReferenceId, SourceEntry, ElseGroup, ConditionTypeOrReference, ConditionValue1, ConditionValue2, NegativeCondition) VALUES
 (25, 638, 0, 47, 14396, 74, 1),
 (25, 655, 0, 47, 14396, 74, 0),
