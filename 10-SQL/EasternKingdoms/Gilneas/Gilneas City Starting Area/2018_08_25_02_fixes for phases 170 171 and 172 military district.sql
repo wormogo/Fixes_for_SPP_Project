8e9cac4b5eac74a36f32f9067a2363474d6a9db4
Monady <vladroman@ymail.com>
2018-09-19 18:41:29 +0300

    fixes for gilneas duskhaven ans city

--- 10-SQL/EasternKingdoms/Gilneas/Gilneas City Starting Area/2018_08_25_02_fixes for phases 170 171 and 172 military district.sql
+++ 10-SQL/EasternKingdoms/Gilneas/Gilneas City Starting Area/2018_08_25_02_fixes for phases 170 171 and 172 military district.sql
@@ -1,4 +1,3 @@
-
 -- fix - quest 14087 14159 14239 phase corections for applying and removeing phases 170 171 and 172 military district
 
 DELETE FROM `spell_area` WHERE `spell`=59073 AND `area`=4757 AND `quest_start`=0 AND `aura_spell`=0 AND `teamId`=-1 AND `racemask`=0 AND `gender`=2;
@@ -12,9 +11,7 @@ INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_end`, `aura_spe
 VALUES (59073, 4757, 14078, 14159, 0, -1, 0, 2, 3, 64, 64);
 
 DELETE FROM `spell_area` WHERE `spell`=59074 AND `area`=4757 AND `quest_start`=14159 AND `aura_spell`=0 AND `teamId`=-1 AND `racemask`=0 AND `gender`=2;
-INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_end`, `aura_spell`, `teamId`, `racemask`, `gender`, `flags`, `quest_start_status`, `quest_end_status`) 
-VALUES (59074, 4757, 14159, 14293, 0, -1, 0, 2, 3, 64, 64);
 
 DELETE FROM `spell_area` WHERE `spell`=59087 AND `area`=4757 AND `quest_start`=14293 AND `aura_spell`=0 AND `teamId`=-1 AND `racemask`=0 AND `gender`=2;
 INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_end`, `aura_spell`, `teamId`, `racemask`, `gender`, `flags`, `quest_start_status`, `quest_end_status`) 
-VALUES (59087, 4757, 14293, 0, 0, -1, 0, 2, 3, 64, 0);
+VALUES (59087, 4757, 14293, 14222, 0, -1, 0, 2, 3, 64, 64);
